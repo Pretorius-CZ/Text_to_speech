3f96ded9141879b2e68c262ba62b94136493b2f1
MichalMalich <flambiz.cz@gmail.com>
2024-06-18 10:47:13 +0200

    Finished app

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from gtts import gTTS
 import os
 from tkinter import *
@@ -6,18 +5,18 @@ from tkinter import ttk, font
 import tkinter as tk
 
 
-# Main window
 root = Tk()
 root.title("Text na řeč")
 root.geometry("400x400")
 root.resizable (False, False)
 root.iconbitmap("icon.ico")
 
-# Fonts
+#Fonty
 custom_font_middle = font.Font(family="Helvetica", size=12)
 custom_font_big = font.Font(family="Helvetica", size=16)
 
-# Read Function
+
+#Fuknce
 def translate(entry, language, audio_filename):
     try:
         text_to_audio = entry
@@ -27,19 +26,18 @@ def translate(entry, language, audio_filename):
     except Exception as e:
         print (f"Chyba: {e}")
 
-
-# Header
+#Nadpis
 main_label = Label (text="Konvertor z řeči na text", font=custom_font_big)
 main_label.grid(row=0, column=0, columnspan=2, pady=10)
 
 empty_label = Label(text=" ")
 empty_label.grid(row=1, column=0)
 
-# Language section
+#Language section
 lang_label = Label (text="Vyberte jazyk výstupu: ", font=custom_font_middle)
 lang_label.grid (row=2, column=0)
 
-# Drop-down
+#Roletka
 lang_drop_down = ttk.Combobox(state="readonly",
                               values=["cs", "en"],
                               width = 33)
@@ -48,7 +46,7 @@ lang_drop_down.grid (row=2, column=1)
 empty_label = Label(text=" ")
 empty_label.grid(row=4, column=0)
 
-# Text section
+#Textová sekce
 input_label = Label (text="Zadejte text pro převod", font=custom_font_middle)
 input_label.grid (row=5, column=0)
 
@@ -58,9 +56,21 @@ input_entry.grid (row=5, column=1)
 empty_label = Label(text=" ")
 empty_label.grid(row=6, column=0)
 
-# Audio filename section
+#Audio filename sekce
 audio_label = Label (text="Název souboru (MP3): ", font=custom_font_middle)
 audio_label.grid(row=7, column=0)
 
 audio_entry = Entry(width = 36)
 audio_entry.grid(row=7, column=1)
+
+
+
+#Button na převod
+button = Button(text="Přečti", font=custom_font_big, command=lambda:translate(input_entry.get("1.0", tk.END).strip(), lang_drop_down.get(), audio_entry.get()))
+button.grid(row=9, column=0, columnspan=2, pady=10)
+
+
+
+root.mainloop()
+
+
