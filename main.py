13de6c008bb0e14ff9c02dab06c7af33ee693eff
MichalMalich <flambiz.cz@gmail.com>
2024-06-18 10:45:14 +0200

    Read function and button created

--- main.py
+++ main.py
@@ -6,29 +6,40 @@ from tkinter import ttk, font
 import tkinter as tk
 
 
-#Main window
+# Main window
 root = Tk()
 root.title("Text na řeč")
 root.geometry("400x400")
 root.resizable (False, False)
 root.iconbitmap("icon.ico")
 
-#Fonts
+# Fonts
 custom_font_middle = font.Font(family="Helvetica", size=12)
 custom_font_big = font.Font(family="Helvetica", size=16)
 
-#Header
+# Read Function
+def translate(entry, language, audio_filename):
+    try:
+        text_to_audio = entry
+        output = gTTS(text=text_to_audio, lang=language, slow=False)
+        output.save(audio_filename + ".mp3")
+        os.system(f"start {audio_filename}.mp3")
+    except Exception as e:
+        print (f"Chyba: {e}")
+
+
+# Header
 main_label = Label (text="Konvertor z řeči na text", font=custom_font_big)
 main_label.grid(row=0, column=0, columnspan=2, pady=10)
 
 empty_label = Label(text=" ")
 empty_label.grid(row=1, column=0)
 
-#Language section
+# Language section
 lang_label = Label (text="Vyberte jazyk výstupu: ", font=custom_font_middle)
 lang_label.grid (row=2, column=0)
 
-#Roletka
+# Drop-down
 lang_drop_down = ttk.Combobox(state="readonly",
                               values=["cs", "en"],
                               width = 33)
@@ -37,7 +48,7 @@ lang_drop_down.grid (row=2, column=1)
 empty_label = Label(text=" ")
 empty_label.grid(row=4, column=0)
 
-#Text section
+# Text section
 input_label = Label (text="Zadejte text pro převod", font=custom_font_middle)
 input_label.grid (row=5, column=0)
 
@@ -47,7 +58,7 @@ input_entry.grid (row=5, column=1)
 empty_label = Label(text=" ")
 empty_label.grid(row=6, column=0)
 
-#Audio filename section
+# Audio filename section
 audio_label = Label (text="Název souboru (MP3): ", font=custom_font_middle)
 audio_label.grid(row=7, column=0)
 
